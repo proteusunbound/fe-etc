b30c2410f055dafd574545185b63ae584bf0d11f
jhfu1121@gmail.com <jhfu1121@gmail.com>
2023-12-02 21:23:12 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,3 +10,18 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def custombutton_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.customization.visible = True
+
+  def unit_drop_change(self, **event_args):
+    """This method is called when an item is selected"""
+    unit = app_tables.unit_stats.get(Name=self.unit_drop.selected_value)
+    self.hp.text = unit['HP']
+    self.strength.text = unit['Str']
+    self.skill.text = unit['Skl']
+    self.speed.text = unit['Spd']
+    self.luck.text = unit['Lck']
+    self.defense.text = unit['Def']
+    self.resistance.text = unit['Res']
