994a16d7b8aed57f2c4e9efc1aaf003458ffc785
jhfu1121@gmail.com <jhfu1121@gmail.com>
2023-12-03 00:50:46 +0000

    Edited Form2 and unit_panel

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -12,3 +12,12 @@ class Form2(Form2Template):
     self.duel = combat.data.DuelSim()
 
     # Any code you write here will run before the form opens.
+
+  def boss_drop_change(self, **event_args):
+    """This method is called when an item is selected"""
+    self.duel.setboss(self.boss_drop.selected_value)
+    self.hp.text = self.duel.boss.maxhp
+    self.strength.text = self.duel.boss.strength
+    self.skill.text = self.duel.boss.skill
+    self.speed.text = self.duel.boss.speed
+    self.defense.text = self.duel.boss.defense
--- client_code/unit_panel/__init__.py
+++ client_code/unit_panel/__init__.py
@@ -18,15 +18,15 @@ class unit_panel(unit_panelTemplate):
 
   def unit_drop_change(self, **event_args):
     """This method is called when an item is selected"""
-    self.duel.setunit(self.unit_drop.selected_value)
-    self.hp.text = self.duel.unit.maxhp
-    self.strength.text = self.duel.unit.strength
-    self.skill.text = self.duel.unit.skill
-    self.speed.text = self.duel.unit.speed
-    self.luck.text = self.duel.unit.luck
-    self.defense.text = self.duel.unit.defense
-    self.resistance.text = self.duel.unit.resistance
-    self.startinghp.text = self.duel.unit.maxhp
+    self.parent.duel.setunit(self.unit_drop.selected_value)
+    self.hp.text = self.parent.duel.unit.maxhp
+    self.strength.text = self.parent.duel.unit.strength
+    self.skill.text = self.parent.duel.unit.skill
+    self.speed.text = self.parent.duel.unit.speed
+    self.luck.text = self.parent.duel.unit.luck
+    self.defense.text = self.parent.duel.unit.defense
+    self.resistance.text = self.parent.duel.unit.resistance
+    self.startinghp.text = self.parent.duel.unit.maxhp
 
   def hide_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -34,25 +34,25 @@ class unit_panel(unit_panelTemplate):
 
   def weapon_drop_change(self, **event_args):
     """This method is called when an item is selected"""
-    self.duel.setunitweapon(self.weapon_drop.selected_value)
-    self.duel.unitdisplay()
-    self.attackspeed.text = self.duel.unit.AS
-    self.hit.text = self.duel.unit.hit
-    self.atk.text = self.duel.unit.attack
-    self.crit.text = self.duel.unit.crit
+    self.parent.duel.setunitweapon(self.weapon_drop.selected_value)
+    self.parent.duel.unitdisplay()
+    self.attackspeed.text = self.parent.duel.unit.AS
+    self.hit.text = self.parent.duel.unit.hit
+    self.atk.text = self.parent.duel.unit.attack
+    self.crit.text = self.parent.duel.unit.crit
 
   def startinghp_change(self, **event_args):
     """This method is called when the text in this text box is edited"""
-    self.duel.setunithp(self.startinghp.select())
+    self.parent.duel.setunithp(self.startinghp.select())
 
   def avoid_drop_change(self, **event_args):
     """This method is called when an item is selected"""
-    self.duel.setavoidno(self.avoid_drop.selected_value)
+    self.parent.duel.setavoidno(self.avoid_drop.selected_value)
 
   def crit_drop_change(self, **event_args):
     """This method is called when an item is selected"""
-    self.duel.setcritno(self.crit_drop.selected_value)
+    self.parent.duel.setcritno(self.crit_drop.selected_value)
 
   def dodge_drop_change(self, **event_args):
     """This method is called when an item is selected"""
-    self.duel.setddgno(self.dodge_drop.selected_value)
+    self.parent.duel.setddgno(self.dodge_drop.selected_value)
