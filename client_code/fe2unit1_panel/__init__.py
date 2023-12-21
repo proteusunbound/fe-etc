6065f695d4bd79a8d3e02629604f31c4038bab78
jhfu1121@gmail.com <jhfu1121@gmail.com>
2023-12-21 21:39:59 +0000

    Edited fe2unit1_panel

--- client_code/fe2unit1_panel/__init__.py
+++ client_code/fe2unit1_panel/__init__.py
@@ -30,6 +30,7 @@ class fe2unit1_panel(fe2unit1_panelTemplate):
         self.startinghp.text = self.parent.combat.duels[0].unit.maxhp
         self.weapon_drop.selected_value = None
         self.weapon_drop.visible = True
+        self.promodrop.selected_value = None
 
     def hide_click(self, **event_args):
         """This method is called when the button is clicked"""
@@ -58,6 +59,7 @@ class fe2unit1_panel(fe2unit1_panelTemplate):
         """Reset"""
         self.unit_drop.selected_value = None
         self.weapon_drop.selected_value = None
+        self.promodrop.selected_value = None
         self.visible = False
         self.customization.visible = False
 
@@ -98,6 +100,20 @@ class fe2unit1_panel(fe2unit1_panelTemplate):
       """This method is called when the user presses Enter in this text box"""
       self.parent.combat.duels[0].unit.boostdefense(self.defshrine.text)
       self.defense.text = self.parent.combat.duels[0].unit.defense
+
+    def promodrop_change(self, **event_args):
+      """This method is called when an item is selected"""
+      self.parent.combat.duels[0].unit.promote(self.promodrop.selected_value)
+      self.hp.text = self.parent.combat.duels[0].unit.maxhp
+      self.strength.text = self.parent.combat.duels[0].unit.strength
+      self.skill.text = self.parent.combat.duels[0].unit.skill
+      self.speed.text = self.parent.combat.duels[0].unit.speed
+      self.defense.text = self.parent.combat.duels[0].unit.defense
+      self.startinghp.text = self.parent.combat.duels[0].unit.maxhp
+      self.parent.combat.duels[0].unitdisplay()
+      self.hit.text = self.parent.combat.duels[0].unit.hit
+      self.crit.text = self.parent.combat.duels[0].unit.crit
+      self.attackspeed.text = self.parent.combat.duels[0].unit.AS
 
 
 
