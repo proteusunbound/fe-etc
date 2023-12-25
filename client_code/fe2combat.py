ed4b1fcff3e462f8a85bfe1c7eb7cc1b9f6ade5c
jhfu1121@gmail.com <jhfu1121@gmail.com>
2023-12-25 19:14:45 +0000

    Edited fe2combat

--- client_code/fe2combat.py
+++ client_code/fe2combat.py
@@ -39,6 +39,12 @@ class CombatSim:
                 self.text += name.dueltext
                 name.reset_text()
         for number, name in self.duels.items():
+            if self.bosshp > 0 and name.unit.hitpoints > 0:
+                if name.boss.maxhp > self.bosshp and name.terrain is True:
+                    self.bosshp = min(self.bosshp + 5, name.boss.maxhp)
+                    self.text += f"{name.boss.name} heals to {self.bosshp} HP at the start of the round.\n"
+                    break
+        for number, name in self.duels.items():
             if (
                 name.unit.hitpoints > 0
                 and self.bosshp > 0
