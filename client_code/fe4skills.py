32ed19f2e768c69be0e60209a3405d267a86ae58
jhfu1121@gmail.com <jhfu1121@gmail.com>
2024-01-16 21:05:51 +0000

    Created fe4skills

---
+++ client_code/fe4skills.py
@@ -0,0 +1,18 @@
+"""Skills"""
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+skill_list = {
+  "Kutuzov": ["Follow-Up"],
+  "Judah": ["Follow-Up"],
+  "Travant": ["Follow-Up"],
+  "Hilda (Chronos)": ["Follow-Up"],
+  "Riddell": ["Follow-Up"],
+  "Zagam": ["Follow-Up"],
+  "Juphiel": ["Follow-Up"],
+  "Brian": ["Follow-Up"],
+  "Hilda (Friege)": ["Follow-Up"],
+  "Manfroy": ["Follow-Up"]
+}
