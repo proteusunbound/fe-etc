9d6ab188c7b8931add465fc879c06b0cea16a2ea
jhfu1121@gmail.com <jhfu1121@gmail.com>
2024-01-16 22:23:52 +0000

    Edited fe4skills

--- client_code/fe4skills.py
+++ client_code/fe4skills.py
@@ -5,14 +5,17 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 skill_list = {
+  "Jacobi": ["Follow-Up"],
+  "Iucharba": ["Accost"],
   "Kutuzov": ["Follow-Up"],
   "Judah": ["Follow-Up"],
   "Travant": ["Follow-Up"],
   "Hilda (Chronos)": ["Follow-Up"],
-  "Riddell": ["Follow-Up"],
+  "Riddell": ["Follow-Up", "Critical"],
   "Zagam": ["Follow-Up"],
   "Juphiel": ["Follow-Up"],
-  "Brian": ["Follow-Up"],
+  "Brian": ["Follow-Up", "Accost"],
   "Hilda (Friege)": ["Follow-Up"],
-  "Manfroy": ["Follow-Up"]
+  "Manfroy": ["Follow-Up"],
+  "Julius": ["Accost"]
 }
