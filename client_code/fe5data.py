81d89f1d7bb23ebbe1e6f1e1d39313d85685b96b
jhfu1121@gmail.com <jhfu1121@gmail.com>
2024-04-18 20:48:48 +0000

    Edited fe5data

--- client_code/fe5data.py
+++ client_code/fe5data.py
@@ -35,3 +35,24 @@ class ActiveWeapon:
         self.might = weapon["Mgt"]
         self.weight = weapon["Wgt"]
         self.hit = weapon["Hit"]
+
+@anvil.server.portable_class
+class ActiveBoss:
+    """Active Boss"""
+    def __init__(self, keyword):
+        boss = app_tables.fe5_boss_stats.get(Name=keyword)
+        self.name = boss["Name"]
+        self.maxhp = boss["HP"]
+        self.strength = boss["Str"]
+        self.magic = boss["Mag"]
+        self.skill = boss["Skl"]
+        self.speed = boss["Spd"]
+        self.luck = boss["Lck"]
+        self.defense = boss["Def"]
+        self.hitpoints = 0
+        self.doubles = False
+        self.damage = 0
+        self.hit = 0
+        self.attack = 0
+        self.avoid = 0
+        self.hitchance = 0
