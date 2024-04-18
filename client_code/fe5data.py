c1f20fb11bee70172fd822f9f4bae823bff0cd6f
jhfu1121@gmail.com <jhfu1121@gmail.com>
2024-04-18 18:27:51 +0000

    Created fe5data

---
+++ client_code/fe5data.py
@@ -0,0 +1,27 @@
+"""Data"""
+
+import math
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+@anvil.server.portable_class
+class ActiveUnit:
+    """Active Unit"""
+
+    def __init__(self, keyword):
+        self.char = app_tables.fe5_unit_stats.get(Name=keyword)
+        self.name = self.char["Name"]
+        self.maxhp = self.char["HP"]
+        self.strength = self.char["Str"]
+        self.magic = self.char["Mag"]
+        self.skill = self.char["Skl"]
+        self.speed = self.char["Spd"]
+        self.defense = self.char["Def"]
+        self.luck = self.char["Lck"]
+        self.hitpoints = 0
+        self.doubles = False
+        self.damage = 0
+        self.hit = 0
+        self.attack = 0
