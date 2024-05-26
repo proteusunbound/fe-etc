40bf09d0d51b942fb4d693e1de421f398d5862fb
jhfu1121@gmail.com <jhfu1121@gmail.com>
2024-05-26 21:02:08 +0000

    Edited fe5skills

--- client_code/fe5skills.py
+++ client_code/fe5skills.py
@@ -5,11 +5,14 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 skills_list = {
+    "Perne": ["Vantage"],
     "Veld": ["Adept"],
     "Leif": ["Adept"],
     "Finn": ["Miracle"],
+    "Halvan": ["Vantage"],
     "Eyvel": ["Adept"],
     "Ronan": ["Adept"],
+    "Machyua": ["Vantage"],
     "Asbel": ["Adept"],
     "Ilios": ["Miracle"],
     "Sara": ["Miracle"],
