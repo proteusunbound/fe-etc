4c3ddb704323416b854d16d5999be88b9f17dca4
jhfu1121@gmail.com <jhfu1121@gmail.com>
2024-07-12 20:47:10 +0000

    Edited fe4skills

--- client_code/fe4skills.py
+++ client_code/fe4skills.py
@@ -126,7 +126,7 @@ skill_list = {
     "Lester": {
       "Jamke": ["Accost", "Adept"],
       "Naoise": ["Accost", "Critical"],
-      
+      "Alec": ["Nihil", "Follow-Up"]
     },
     "Deimne": ["Follow-Up"],
     "Oifey": ["Follow-Up", "Critical"],
